63181c1628eff3ed00c80d425a2514a7e12e3980
HARSH KUMAR <119001770+harshkumarhky@users.noreply.github.com>
2024-08-28 20:47:28 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -11,3 +11,48 @@ public class swapping{
         System.out.println("After Swapping: a = " + a +", b = " +b);
     }
 }
+
+
+2.//Calculator//
+
+
+import java.util.*;
+public class calculator {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the first number: ");
+    double num1 = sc.nextDouble();
+    System.out.println("Enter the Operator: +; -; *; /;");
+    char Operator = sc.next().charAt(0);
+    System.out.println("Enter the second number:");
+    double num2 = sc.nextDouble();
+    double result = 0;
+    if(Operator == '+'){
+        result = num1 + num2;
+    }else if(Operator == '-'){
+        result = num1 - num2;
+    }else if(Operator =='*'){
+        result = num1 * num2;
+    }else if(Operator == '/'){
+        result = num1 / num2;
+    }else {
+        System.out.println("Invalid Operator");
+        return;
+    }
+    System.out.println("Result: " + result);
+}  
+}    
+
+3. // Factorial //
+
+public class factorial{
+    public static void main(String[] args) {
+        int a = 5; 
+        int factorial = 1;
+        for (int i = 1; i <= a; i++) {
+            factorial *= i;
+        }
+        System.out.println("Factorial of " + a + " is " + factorial);
+    }
+}    
+    
