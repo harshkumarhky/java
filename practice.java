6871ee97c572f5dccbe05342bb604f9cbdc365f7
HARSH KUMAR <119001770+harshkumarhky@users.noreply.github.com>
2024-08-28 15:57:32 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -1,13 +1,13 @@
-public class swapping {
-    public static void main(String[] args) {
+1. //Swapping 2 Numbers//
+
+public class swapping{
+    public static void main(String[] args){
         int a = 5;
         int b = 10;
-
-        System.out.println("Before swapping:" + a +" "+ b);
-        a = a + b; 
-        b = a - b; 
-        a = a - b; 
-
-        System.out.println("After swapping:" + a + " " + b);
+        System.out.println("Before Swapping: a = " + a +", b = "+b);
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println("After Swapping: a = " + a +", b = " +b);
     }
 }
