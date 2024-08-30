f5ceaac1e687268e3d04d5a945b2d78a01bb0418
HARSH KUMAR <119001770+harshkumarhky@users.noreply.github.com>
2024-08-30 22:35:33 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -16,30 +16,45 @@ public class swapping{
 2.//Calculator//
 
 
-import java.util.*;
-public class calculator {
-    public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the first number: ");
-    double num1 = sc.nextDouble();
-    System.out.println("Enter the Operator: +; -; *; /;");
-    char Operator = sc.next().charAt(0);
-    System.out.println("Enter the second number:");
-    double num2 = sc.nextDouble();
-    double result = 0;
-    if(Operator == '+'){
-        result = num1 + num2;
-    }else if(Operator == '-'){
-        result = num1 - num2;
-    }else if(Operator =='*'){
-        result = num1 * num2;
-    }else if(Operator == '/'){
-        result = num1 / num2;
-    }else {
-        System.out.println("Invalid Operator");
-        return;
+import java.util.Scanner;
+
+public class practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        double a = sc.nextDouble();
+
+        System.out.println("Enter the operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        System.out.println("Enter the second number: ");
+        double b = sc.nextDouble();
+
+        double result = 0;
+
+        switch (operator) {
+            case '+':result = a + b;
+                break;
+            case '-':result = a - b;
+                break;
+            case '*':result = a * b;
+                break;
+            case '/':if (b != 0) {
+                    result = a / b;
+                } else {
+                    System.out.println("Error! Division by zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                return;
+        }
+              System.out.println("The result is: " + result);
     }
-    System.out.println("Result: " + result);
+}
+
 }  
 }    
 
@@ -56,3 +71,46 @@ public class factorial{
     }
 }    
     
+4. // For Loop //
+
+    import java.util.Scanner;
+
+public class conditions {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+      
+        if (a == b) {
+            System.out.println("a and b are equal");
+            }
+        else if (a > b) {
+            System.out.println("a is greater than b");
+         } else{
+            System.out.println("b is greater than a");
+        }
+    }    
+}
+
+4. // odd even //
+
+    import java.util.Scanner;
+
+/**
+ * oddeven
+ */
+public class oddeven {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if (x % 2 == 0) {
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }    
+        }
+    }
