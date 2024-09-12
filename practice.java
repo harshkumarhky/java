a13612382ddcc39f709e43caadaf26a748bfc475
HARSH KUMAR <119001770+harshkumarhky@users.noreply.github.com>
2024-09-12 16:54:17 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -111,6 +111,24 @@ public class Table {
 }
 
 --------------------------------------------------------------------------------------------
+8. Fibonacci series
+    
+    import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+        int a = 0, b = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.println(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+              }
+      }
+    }
+------------------------------------------------------------------------------------------------
 //Swapping 2 Numbers//
 
 public class swapping{
